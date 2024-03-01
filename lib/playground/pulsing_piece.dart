ba6c4e18b5c5b896154e0fb83344bbc3222c68a6
Andrew <agthatch@gmail.com>
2024-03-01 00:39:51 -0700

    make staged pieces Shimmer!

--- lib/playground/pulsing_piece.dart
+++ lib/playground/pulsing_piece.dart
@@ -1,6 +1,8 @@
 import 'package:card/game_internals/piece/playing_piece.dart';
+import 'package:card/play_session/piece_color_getter.dart';
 import 'package:card/play_session/playing_piece_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class PulsingPieceWidget extends StatefulWidget {
   final PlayingPiece piece;
@@ -11,56 +13,24 @@ class PulsingPieceWidget extends StatefulWidget {
   PulsingPieceWidgetState createState() => PulsingPieceWidgetState();
 }
 
-class PulsingPieceWidgetState extends State<PulsingPieceWidget>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
+class PulsingPieceWidgetState extends State<PulsingPieceWidget> {
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1), // Adjust duration as needed
-    );
-
-    _animation = Tween<double>(
-      begin: 1.0, // Start from 50% opacity
-      end: 0.5, // End at 100% opacity
-    ).animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut, // Use a linear curve
-    ));
-
-    // Start animation when the widget is first built
-    if (widget.piece.isStaged) {
-      _controller.repeat(reverse: true);
-    }
-  }
-
-  @override
-  void didUpdateWidget(covariant PulsingPieceWidget oldWidget) {
-    super.didUpdateWidget(oldWidget);
-
-    // Check if animation needs to be started or stopped based on the boolean value
-    if (widget.piece.isStaged) {
-      _controller.repeat(reverse: true);
-    } else {
-      _controller.reset();
-    }
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return FadeTransition(
-      opacity: _animation,
-      child: Center(child: PlayingPieceWidget(widget.piece)),
-    );
+    if (widget.piece.isStaged) {
+      return Shimmer.fromColors(
+        period: Duration(milliseconds: 2500),
+        baseColor:
+            PieceColorGetter.get(widget.piece.shape, context).withAlpha(200),
+        highlightColor: Color.fromARGB(118, 77, 77, 77),
+        child: Center(child: PlayingPieceWidget(widget.piece)),
+      );
+    } else {
+      return PlayingPieceWidget(widget.piece);
+    }
   }
 }
